8b75e483c20b79f33e3833cf7c4f4210a80d468e
Karthik_Raja <lkarthikaraja@gmail.com>
2026-07-02 11:04:44 +0530

    deploy changes

--- app/main.py
+++ app/main.py
@@ -1,28 +1,36 @@
 from fastapi import FastAPI
 
-from app.api.health import router as health_router
-from app.api.chat import router as chat_router
-from app.api.image import router as image_router
-from app.api.vision import router as vision_router
-
 app = FastAPI(
     title="Backend Chatbot API",
     version="1.0.0",
     description="Python Backend Engineer Assignment"
 )
 
+# --------------------
+# Routers
+# --------------------
+from app.api.health import router as health_router
+from app.api.chat import router as chat_router
+from app.api.image import router as image_router
+from app.api.vision import router as vision_router
+from app.api import home
+
 app.include_router(health_router)
 app.include_router(chat_router)
 app.include_router(image_router)
 app.include_router(vision_router)
+app.include_router(home.router)
 
-
+# --------------------
+# Root
+# --------------------
 @app.get("/")
 async def root():
-    return {
-        "message": "Welcome to Backend Chatbot API"
-    }
-    
+    return {"message": "Backend Chatbot API"}
+
+# --------------------
+# Exceptions
+# --------------------
 from app.exceptions.callmissed import CallMissedAPIException
 from app.exceptions.handlers import (
     callmissed_exception_handler,
@@ -38,10 +46,3 @@ app.add_exception_handler(
     Exception,
     generic_exception_handler
 )
-
-from fastapi.staticfiles import StaticFiles
-from app.api import home
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-app.include_router(home.router)
